07e574a54813ba42266fd578fd48d34031ac1a2d
SebastianBentley <56867923+SebastianBentley@users.noreply.github.com>
2019-12-05 12:26:04 +0100

    Add files via upload

---
+++ InsertDefaultValues.sql
@@ -0,0 +1,27 @@
+INSERT INTO dolphinclub.team
+VALUES (team_id, 'ungdomshold', 'Raschael');
+INSERT INTO dolphinclub.team
+VALUES (team_id, 'seniorhold', 'Sebahstian');
+
+INSERT INTO dolphinclub.pricelist
+VALUES (m_type, 500);
+INSERT INTO dolphinclub.pricelist
+VALUES (m_type, 1000);
+INSERT INTO dolphinclub.pricelist
+VALUES (m_type, 1200);
+INSERT INTO dolphinclub.pricelist
+VALUES (m_type, 1600);
+
+INSERT INTO dolphinclub.training
+VALUES (t_id, 'Raschael');
+INSERT INTO dolphinclub.training
+VALUES (t_id, 'Sebahstian');
+
+INSERT INTO dolphinclub.discipline
+VALUES (d_id, 'CRAWL');
+INSERT INTO dolphinclub.discipline
+VALUES (d_id, 'BACKSTROKE');
+INSERT INTO dolphinclub.discipline
+VALUES (d_id, 'BREASTSTROKE');
+INSERT INTO dolphinclub.discipline
+VALUES (d_id, 'BUTTERFLY');
